8f5abd90a65fdd51832d7bfede5d72149bc7259b
unintellectual-hypothesis <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-25 01:11:05 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,4 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+VAR_DIR="$MODDIR/variants"
+MEM_TOTAL="$(awk '/^MemTotal:/{print $2}' /proc/meminfo)"
 
-
+if [ "$MEM_TOTAL" -le 3145728 ]; then
+    sh $VAR_DIR/3G_main.sh
+elif [ "$MEM_TOTAL" -le 4197304 ]; then
+    sh $VAR_DIR/4G_main.sh
+else
+    sh $VAR_DIR/6G_more_main.sh
+fi
