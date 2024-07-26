380bdd7fa01e780574a8ea0c57cf67ac5ef24884
비지적 가설 <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-26 05:21:55 +0000

    Updated mem-features

---
+++ mem-features/kswapd_oom_aff.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+# THANKS TO yc9559 @ GitHub
+MODDIR=${0%/*}
+
+# Load libraries 
+MEM_FEATURES_DIR="$MODULE_PATH/mem-features"
+. "$MEM_FEATURES_DIR"/paths.sh
+
+change_task_affinity()
+{
+    local ps_right
+    ps_right="$(ps -Ao pid,args)"
+    for temp_pid in $(echo "$ps_right" | grep "$1" | awk '{print $1}'); do
+        for temp_tid in $(ls "/proc/$temp_pid/task/"); do
+            taskset -p -- "7f" "$temp_tid"
+        done
+    done
+}
+
+change_task_nice()
+{
+    local ps_right
+    ps_right="$(ps -Ao pid,args)"
+    for temp_pid in $(echo "$ps_right" | grep "$1" | awk '{print $1}'); do
+        for temp_tid in $(ls "/proc/$temp_pid/task/"); do
+            renice -p -n "-2" "$temp_tid"
+        done
+    done
+}
