927af1cec0b2cfcdc2fe84f612e5b0be06625655
비지적 가설 <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-26 04:06:28 +0000

    Update dynamic swappiness

--- mem-features/dynamic_swappiness.sh
+++ mem-features/dynamic_swappiness.sh
@@ -1,65 +1,69 @@
 #!/system/bin/sh
 # THANKS TO VR-25 @ GitHub
+MODDIR=${0%/*}
 
-# load libraries
-CURR_DIR="$(dirname "$0")"
-. "$CURR_DIR"/PATHS.sh
+# Load libraries 
+MEM_FEATURES_DIR="$MODULE_PATH/mem-features"
+. "$MEM_FEATURES_DIR"/paths.sh
 
 high_load_threshold=""
 medium_load_threshold=""
-swap_over_hundy=0
+swappiness_change_rate=""
+swap_over_hundy=""
 
-# Test if kernel supports swappiness over 100 (Some ROM defaults swappiness to 100)
+# Test if system supports swappiness over 100 (Some ROM defaults swappiness to 100)
 test_swappiness()
 {
-  set_value "160" "$VM"/swappiness
-  new_swappiness=$(cat "$VM"/swappiness)
-  if [ "$new_swappiness" -eq 160 ]; then
-    swap_over_hundy=1
-  else
-    swap_over_hundy=0
-  fi
+    set_val "180" "$VM"/swappiness
+    new_swappiness="$(cat "$VM"/swappiness)"
+    if [ "$new_swappiness" -eq 180 ]; then
+        swap_over_hundy=1
+    else
+        swap_over_hundy=0
+    fi
 }
+
 
 # Dynamic swappiness & vfs_cache_pressure iff Swap Exists based on /proc/loadavg
 start_dynamic_swappiness()
 {
-  # Default threshold values
-  high_load_threshold="$(read_cfg high_load_threshold)"
-  [ "$high_load_threshold" == "" ] && high_load_threshold=50
-  medium_load_threshold="$(read_cfg medium_load_threshold)"
-  [ "$medium_load_threshold" == "" ] && medium_load_threshold=25
+    high_load_threshold="$(read_cfg high_load_threshold)"
+    [ "$high_load_threshold" == "" ] && high_load_threshold=50
+    medium_load_threshold="$(read_cfg medium_load_threshold)"
+    [ "$medium_load_threshold" == "" ] && medium_load_threshold=25
+    swappiness_change_rate="$(read_cfg swappiness_change_rate)"
+    [ "$swappiness_change_rate" == "" ] && swappiness_change_rate=15
 
-  while [ "$(awk '/^SwapTotal:/{print $2}' /proc/meminfo)" != 0 ]; do
-  load_avg=$(awk  '{printf "%.0f", ($1 * 100 / 8)}'  /proc/loadavg)
-    if [ "$load_avg" -ge "$(read_cfg high_load_threshold)" ]; then
-      resetprop -n ro.lmk.use_minfree_levels false
-      if [ "$swap_over_hundy" -eq 1 ]; then
-        set_value "100" "$VM"/swappiness
-        set_value "175" "$VM"/vfs_cache_pressure
-      elif [ "$swap_over_hundy" -eq 0 ]; then
-        set_value "85" "$VM"/swappiness
-        set_value "175" "$VM"/vfs_cache_pressure
-      fi
-    elif [ "$load_avg" -ge "$(read_cfg medium_load_threshold)" ]; then
-      resetprop -n ro.lmk.use_minfree_levels true
-      if [ "$swap_over_hundy" -eq 1 ]; then
-        set_value "165" "$VM"/swappiness
-        set_value "140" "$VM"/vfs_cache_pressure
-      elif [ "$swap_over_hundy" -eq 0 ]; then
-        set_value "90" "$VM"/swappiness
-        set_value "140" "$VM"/vfs_cache_pressure
-      fi
-    elif [ "$load_avg" -ge 0 ]; then
-      resetprop -n ro.lmk.use_minfree_levels true
-      if [ "$swap_over_hundy" -eq 1 ]; then
-        set_value "180" "$VM"/swappiness
-        set_value "110" "$VM"/vfs_cache_pressure
-      elif [ "$swap_over_hundy" -eq 0 ]; then
-        set_value "100" "$VM"/swappiness
-        set_value "110" "$VM"/vfs_cache_pressure
-      fi
-    fi
-    sleep 5
+    while [ "$(awk '/^SwapTotal:/{print $2}' /proc/meminfo)" != 0 ]; do
+    load_avg=$(awk  '{printf "%.0f", ($1 * 100 / 8)}'  /proc/loadavg)
+        if [ "$load_avg" -ge "$(read_cfg high_load_threshold)" ]; then
+            resetprop -n ro.lmk.use_minfree_levels false
+            if [ "$swap_over_hundy" -eq 1 ]; then
+                set_val "100" "$VM"/swappiness
+                set_val "175" "$VM"/vfs_cache_pressure
+            elif [ "$swap_over_hundy" -eq 0 ]; then
+                set_val "90" "$VM"/swappiness
+                set_val "175" "$VM"/vfs_cache_pressure
+            fi
+        elif [ "$load_avg" -ge "$(read_cfg medium_load_threshold)" ]; then
+            resetprop -n ro.lmk.use_minfree_levels true
+            if [ "$swap_over_hundy" -eq 1 ]; then
+                set_val "165" "$VM"/swappiness
+                set_val "140" "$VM"/vfs_cache_pressure
+            elif [ "$swap_over_hundy" -eq 0 ]; then
+                set_val "95" "$VM"/swappiness
+                set_val "140" "$VM"/vfs_cache_pressure
+            fi
+        elif [ "$load_avg" -ge 0 ]; then
+            resetprop -n ro.lmk.use_minfree_levels true
+            if [ "$swap_over_hundy" -eq 1 ]; then
+                set_val "180" "$VM"/swappiness
+                set_val "110" "$VM"/vfs_cache_pressure
+            elif [ "$swap_over_hundy" -eq 0 ]; then
+                set_val "100" "$VM"/swappiness
+                set_val "110" "$VM"/vfs_cache_pressure
+            fi
+        fi
+    sleep "$(read_cfg swappiness_change_rate)"
   done &
 }
