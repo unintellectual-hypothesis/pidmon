4a9e63873adccc404031e61b856089ed659d95e1
비지적 가설 <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-26 10:32:32 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 
+# Remove swapfile
 rm -rf "data/lswap/swapfile"
 
 # Wait until write is permissible on /sdcard
@@ -7,4 +8,5 @@ while [ ! -d "/sdcard/Android" ]; do
   sleep 1
 done
 
+# Remove config file
 rm -rf "/sdcard/Android/fog_mem_config.txt"
