39dd6f561994fe6c856fa854a949d766c76d245a
비지적 가설 <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-31 14:17:52 +0700

    v3.0

--- src/intelligent_zram_writeback.cpp
+++ src/intelligent_zram_writeback.cpp
@@ -198,5 +198,36 @@ void startAutoZRAMwriteback() {
 }
 
 int main() {
+    pid_t pid, sid;
+
+    pid = fork();
+
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    umask(0);
+
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    signal(SIGTERM, signalHandler);
+
     startAutoZRAMwriteback();
+
+    exit(EXIT_SUCCESS);
 }
