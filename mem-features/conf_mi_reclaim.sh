965308188c431b5e6e297b38b6e0b20152d2f7f1
비지적 가설 <150039884+unintellectual-hypothesis@users.noreply.github.com>
2024-07-31 13:05:54 +0700

    v3.0

--- mem-features/conf_mi_reclaim.sh
+++ mem-features/conf_mi_reclaim.sh
@@ -23,14 +23,14 @@ conf_mi_reclaim() {
         else
         return
         fi
-        chmod 000 $mi_rtmm/reclaim/auto_reclaim 2>/dev/null
-        chown root:root $mi_rtmm/reclaim/auto_reclaim 2>/dev/null
-        chmod 000 $mi_rtmm/reclaim/global_reclaim 2>/dev/null
-        chown root:root $mi_rtmm/reclaim/global_reclaim 2>/dev/null
-        chmod 000 $mi_rtmm/reclaim/proc_reclaim 2>/dev/null
-        chown root:root $mi_rtmm/reclaim/proc_reclaim 2>/dev/null
-        chmod 000 $mi_rtmm/reclaim/kill 2>/dev/null
-        chown root:root $mi_rtmm/reclaim/kill 2>/dev/null
-        chown root:root $mi_rtmm/compact/compact_memory 2>/dev/null
+        chmod 000 "$mi_rtmm"/reclaim/auto_reclaim 2>/dev/null
+        chown root:root "$mi_rtmm"/reclaim/auto_reclaim 2>/dev/null
+        chmod 000 "$mi_rtmm"/reclaim/global_reclaim 2>/dev/null
+        chown root:root "$mi_rtmm"/reclaim/global_reclaim 2>/dev/null
+        chmod 000 "$mi_rtmm"/reclaim/proc_reclaim 2>/dev/null
+        chown root:root "$mi_rtmm"/reclaim/proc_reclaim 2>/dev/null
+        chmod 000 "$mi_rtmm"/reclaim/kill 2>/dev/null
+        chown root:root "$mi_rtmm"/reclaim/kill 2>/dev/null
+        chown root:root "$mi_rtmm"/compact/compact_memory 2>/dev/null
   fi
 }
